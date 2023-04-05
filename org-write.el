75400bfae1e81403e0743c1057fc78c79e41ce08
scholablade <mohamed.kabouk@gmail.com>
2023-04-05 14:44:02 +0000

    Commit

--- org-write.el
+++ org-write.el
@@ -1,23 +1,39 @@
-(defcustom org-write-directory "~/org-write"
-    "Directory for org-write"
-    :type 'directory
-:type 'string
-)
-
 (defun org-write-mode ()
-	 (interactive)
-       (dired-create-directory org-write-directory)
-       (let* ((path org-write-directory)
-	      )
-(find-file-noselect path)
-(create-file (concat path "/" "Summary.org") Summary)
-(create-file (concat path "/" "General.org") General)
-(create-file (concat path "/" "World.org" World))
-(create-file (concat path "/" "Plot.org" Plot))
-(create-file (concat path "/" "Editor.org" Editor))
-))
+	   (interactive)
+	 (dired-create-directory org-write-directory)
+	 (let* ((path org-write-directory)
+		)
+  (find-file-noselect path)
+  (create-file (concat path "/" "Summary.org") Summary)
+  (create-file (concat path "/" "General.org") General)
+  (create-file (concat path "/" "World.org" World))
+  (create-file (concat path "/" "Plot.org" Plot))
+  (create-file (concat path "/" "Editor.org" Editor))
+  ))
+  
+abckajc
 
-(defun Characters ()
-  (setq Characters-path (concat path "/Characters/"))
-(dolist (Characters-subdirs '("Main" "Secondary" "Minor"))
-  (make-directory (concat Characters-path "/" Characters-subdirs))))
+(defun create-file (filename content)
+(with-temp-file filename
+  (insert content)))
+
+(setq Characters "* Main \n* Secondary \n* Minor")
+
+(setq Plot "* Main \n* Secondary \n* Minor")
+
+(setq World "* Places \n* Cultures \n* Travel" )
+
+(setq Editor
+" Chapter 1 \n** Section 1 \nSection 2 \nSection 3 \nSection 4 \nSection 5
+* Chapter 2 \n **Section 1\n Section 2 \n Section 3 \n Section 4 \n Section 5
+* Chapter 3 \n ** Section 1 \n Section 2 \n Section 3 \n Section 4 \n Section 5
+* Chapter 4 \n ** Section 1 \n Section 2 \n Section 3 \n Section 4 \n Section 5
+* Chapter 5
+* Chapter  6 \n ** Section 1 \n Section 2 \n Section 3 \n Section 4 \n Section
+* Chapter 7 \n ** Section 1 \n Section 2 \n Section 3 \n Section 4 \n Section 5
+* Chapter 8 \n ** Section 1 \n Section 2 \n Section 3 \n Section 4 \n Section 5
+* Chapter 9 \n ** Section 1 \n Section 2 \n Section 3 \n Section 4 \n Section 5
+* Chapter 10 \n ** Section 1 \n Section 2 \n Section 3 \n Section 4 \n Section 5
+* Chapter 11 \n ** Section 1 \n Section 2 \n Section 3 \n Section 4 \n Section 5
+* Chapter 12 \n ** Section 1 \n Section 2 \n Section 3 \n Section 4 \n Section 5" 
+	       )
